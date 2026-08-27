2170725e5bfddb7f47fb7c6a0035712aadbcbcef
Buggy <buggy7073@gmail.com>
2026-08-27 16:58:19 +0530

    IPL Prediction Project

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st 
+import pickle
+import pandas as pd
+
+
+teams = ['Royal Challengers Bangalore', 'Rajasthan Royals',
+       'Chennai Super Kings', 'Kings XI Punjab', 'Mumbai Indians',
+       'Kolkata Knight Riders', 'Sunrisers Hyderabad', 'Delhi Capitals']
+
+cities = ['Chennai', 'Sharjah', 'Bengaluru', 'Jaipur', 'Mumbai', 'Kimberley',
+       'Centurion', 'Kolkata', 'Indore', 'Visakhapatnam', 'Delhi',
+       'Cape Town', 'Bangalore', 'Hyderabad', 'Chandigarh', 'Raipur',
+       'Durban', 'Ahmedabad', 'Dharamsala', 'Port Elizabeth', 'Abu Dhabi',
+       'Ranchi', 'Cuttack', 'Johannesburg', 'Nagpur', 'Pune', 'Mohali',
+         'Bloemfontein', 'East London']
+
+gb = pickle.load(open("gb.pkl","rb"))
+st.title("IPL Win Predictor")
+
+col1 , col2 = st.columns(2)
+
+with col1:
+    batting_team = st.selectbox("Select the batting team",sorted(teams))
+with col2:
+    bowling_team = st.selectbox("Select the bowling team",sorted(teams))
+
+
+selected_city = st.selectbox("Select host city",sorted(cities))
+
+target = st.number_input("Target")
+
+col3,col4,col5 = st.columns(3)
+
+with col3:
+    score = st.number_input("Score")
+with col4:
+    overs = st.number_input("Overs Completed")
+with col5:
+    wickets = st.number_input("Wickets out")
+
+if st.button("Predict Probablity"):
+    runs_left = target - score
+    balls_left = 120 - (overs*6)
+    wickets = 10 - wickets
+    crr = score/overs
+    rrr = (runs_left*6)/(balls_left + 00000000000.1)
+
+    input_df = pd.DataFrame({"batting_team":[batting_team],"bowling_team":[bowling_team],
+    "city":[selected_city],"runs_left":[runs_left],"balls_left":[balls_left],
+    "wickets":[wickets],"total_runs_x":[target],"crr":[crr],"rrr":[rrr] })
+
+    result = gb.predict_proba(input_df)
+    loss = result[0][0]
+    win = result[0][1]
+
+    st.header(batting_team + "-" + str(round(win * 100)) + "%")
+    st.header(bowling_team + "-" + str(round(loss * 100)) + "%")
+
+
+    
